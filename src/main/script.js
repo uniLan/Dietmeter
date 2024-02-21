74354a51d1707bdec7f021582ce264d78b1197f3
Johnson Lin <unlinnnn19@gmail.com>
2024-02-21 15:53:41 -0500

    finish demo main page

---
+++ src/main/script.js
@@ -0,0 +1,7 @@
+function addActivity() {
+    alert("Here you would open a form or modal to add a new activity.");
+}
+
+function openMealRecording() {
+    alert("Here you would open a form or modal to record a new meal.");
+}
