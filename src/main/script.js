8401a96751b32d0ae4fc555043994a04552f824c
Johnson Lin <unlinnnn19@gmail.com>
2024-03-14 05:45:09 -0400

    adding the echo, still have bug. need to fix

--- src/main/script.js
+++ src/main/script.js
@@ -4,6 +4,9 @@ function addActivity() {
 
 function openMealRecording() {
     alert("Here you would open a form or modal to record a new meal.");
+    openChatWindow()
+    const userInput = sendNewMessage("What you eat? How many?");
+    sendNewMessage("You ate: " + userInput);
 }
 
 function toggleChatWindow() {
@@ -12,6 +15,22 @@ function toggleChatWindow() {
         chatWindow.classList.remove('hiddenCtrl-hide');
         chatWindow.classList.add('hiddenCtrl-show');
     } else {
+        chatWindow.classList.remove('hiddenCtrl-show');
+        chatWindow.classList.add('hiddenCtrl-hide');
+    }
+}
+
+function openChatWindow() {
+    const chatWindow = document.getElementById('chat-window');
+    if (chatWindow.classList.contains('hiddenCtrl-hide')) {
+        chatWindow.classList.remove('hiddenCtrl-hide');
+        chatWindow.classList.add('hiddenCtrl-show');
+    }
+}
+
+function closeChatWindow() {
+    const chatWindow = document.getElementById('chat-window');
+    if (chatWindow.classList.contains('hiddenCtrl-show')) {
         chatWindow.classList.remove('hiddenCtrl-show');
         chatWindow.classList.add('hiddenCtrl-hide');
     }
@@ -39,9 +58,13 @@ function sendMessage() {
         // Scroll the chat-messages div to the bottom
         const chatMessagesContainer = document.getElementById('chat-messages-container');
         chatMessagesContainer.scrollTop = chatMessagesContainer.scrollHeight;
-        
-    }
 
-    
+        return userInput;
+    }
+}
+
+function sendNewMessage(message) {
+    document.getElementById('chat-input').value = message;
+    return sendMessage();
 }
 
